73cb1cd603541043d47c1b98dc493d4ba42c845a
Hirofumi Muramoto <hirofumi.muramoto@gmail.com>
2025-12-30 13:16:51 +0000

    images + updated numerov code

--- updated numerov codes/matrix_numerov.m
+++ updated numerov codes/matrix_numerov.m
@@ -7,9 +7,12 @@ r_box = 30;
 r_start = 1e-5;
 
 % # of points, distance interval, and orbital quantum number designation
-N_points = 2000;
+N_points = 4000;
 d = (r_box-r_start) / (N_points-1);
 l = 0;
+
+% choosing which energy level to investigate up to
+n_lim = 10;
 
 r_points = linspace(r_start, r_box, N_points)';
 
@@ -32,9 +35,8 @@ V_eff_vec = -1 ./ r_points + l .* (l+1) ./ (2 .* r_points .^2);
 
 V_eff = spdiags(V_eff_vec(2:end-1), 0, N_points - 2, N_points -2);
 
-% finding eigenvecs and eignevals, using full() because A and B are sparse
-% matrices
-[eig_vec, D] = eig(full(-1/2 .* A + B * V_eff), full(B));
+% calculating eigenvals and eigenvecs using the eigs function
+[eig_vec, D] = eigs(-1/2 .* A + B * V_eff, B, n_lim, "smallestabs");
 
 % extracting eigenvals from the diagonalized eigenval matrix and ordering
 eig_val = diag(D);
@@ -46,7 +48,23 @@ fprintf('Ground State Energy: %.5f Hartree\n', energies(1));
 
 %% plotting
 
-figure
+figure;
 hold on;
-n_lim = 20;
-plot(1:n_lim, energies(1:n_lim))
+
+% Plot numerical energies with markers
+plot(1:n_lim, energies(1:n_lim), 'x', "MarkerSize", 15, "LineWidth", 2, 'DisplayName', 'Numerical');
+
+% Plot analytical hydrogen energies as a smooth curve
+fplot(@(x) -1./(2*x.^2), [1 n_lim], 'r', 'LineWidth', 1.5, 'DisplayName', 'Analytical (E = -1/(2n^2))');
+
+xlabel('n', 'FontSize', 15);
+ylabel('E (Ha)', 'FontSize', 15);
+title(sprintf('Hydrogen Energy Levels (l = %d, N_{points} = %d, r_{box} = %.2f, E_1 = %.5f Ha)', l, N_points, r_box, energies(1)), 'FontSize', 14);
+legend('Location', 'northwest', "FontSize", 15);
+grid on;
+xlim([1 n_lim]);
+
+% saving
+filename = sprintf('hydrogen_energies_l%d_N%d_r%.2f.png', l, N_points, r_box);
+
+exportgraphics(gcf, filename, 'Resolution', 300);
